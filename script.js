c3f3f0285f53b96c551a96d8163c13f2c583cfb8
Ulvi <ulvi.shabanov@gmail.com>
2025-09-21 01:47:43 +0400

    added functionality of guessing of number

--- script.js
+++ script.js
@@ -1,1 +1,58 @@
 'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct number🏆';
+
+// document.querySelector('.number').textContent = 13;
+
+// document.querySelector('.score').textContent = 10;
+
+// console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 23;
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', () => {
+  const guess = Number(document.querySelector('.guess').value);
+
+  // when there is no input
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number!';
+  }
+
+  // when player wins
+  else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct number!';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+  }
+
+  // when guess is high
+  else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+
+  // when guess is low
+  else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
